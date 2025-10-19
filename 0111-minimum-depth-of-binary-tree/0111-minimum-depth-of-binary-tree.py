5d271885597dfcaa25dbeedc67002dd460de6320
Areeba Hanif <aribahanif010@gmail.com>
2025-10-19 19:38:55 +0500

    Time: 3 ms (79.17%), Space: 49.6 MB (94.4%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -4,19 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
+        # if not root then return 0
         if not root:
             return 0
-        
-        left_depth = self.minDepth(root.left)
-        right_depth = self.minDepth(root.right)
 
-        if not root.left:
-            return 1 + right_depth
-        
-        if not root.right:
-            return 1 + left_depth
-        
-        return 1 + min(left_depth, right_depth)
-        
+        # make a queue, put node and its current depth in it 
+        queue = deque([(root,1)])
+
+        # run a loop until no node in the queue
+        while queue:
+
+        # take current node and depth from the queue
+            current_node, current_depth = queue.popleft()
+
+        # check if this node have not left and right node then it will be a leaf node and our answer
+            if not current_node.left and not current_node.right:
+                return current_depth
+
+         # check if this node have a left child or a right child then put it in the queue 
+            if current_node.left:
+                queue.append((current_node.left, current_depth + 1))
+            if current_node.right:
+                queue.append((current_node.right, current_depth + 1))
